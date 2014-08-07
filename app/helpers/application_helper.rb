ed72ea3ab9055e391f9c653249da21e485a3bbbd
John Dixon <john.dixon.615@gmail.com>
2014-08-06 22:18:58 -0400

    Added Redcarpet

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def markdown(body)
+  		@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true)
+  		@markdown.render(body)
+	end
 end
