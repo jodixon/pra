736aa842f84a6a8a60fab495bac464283e88d828
John Dixon <john.dixon.615@gmail.com>
2014-08-17 05:23:34 -0400

    Added custom routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,19 @@ module ApplicationHelper
   		@markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true)
   		@markdown.render(body)
 	end
+
+	def bootstrap_class_for flash_type
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-error"
+      when :alert
+        "alert-block"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
 end
