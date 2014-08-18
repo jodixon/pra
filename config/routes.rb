5f0bdb145504cca7d9da9a9c36f87722ad9f0c42
John Dixon <john.dixon.615@gmail.com>
2014-08-18 04:38:38 -0400

    Added profile page

--- config/routes.rb
+++ config/routes.rb
@@ -11,12 +11,16 @@ Rails.application.routes.draw do
 
   resources :concepts
 
+  get 'feed', to: 'concepts#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'concepts#index'
 
+
+  get '/:id', to: 'profiles#show'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
